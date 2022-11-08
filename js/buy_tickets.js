c3a2fa467ea7700ea88131241feb6e07cd4c7f03
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2022-11-08 12:18:27 -0300

    buy_tickets_deleteBtn

--- js/buy_tickets.js
+++ js/buy_tickets.js
@@ -1,15 +1,17 @@
-let total_pay = document.getElementById("total_pay");
+let name_input = document.getElementById("name_input");
+let surname_input = document.getElementById("surname_input");
+let email_input = document.getElementById("email_input");
 let tickets_amount = document.getElementById("tickets_amount");
 let discount_select = document.getElementById("discount_select");
+let total_pay = document.getElementById("total_pay");
 
-let total = 0
-let error = 0
-let disc
+let total;
+let error = 0;
+let disc;
 
 function discount(d,error){
     if (d === "student"){
         disc = 0.2;
-        console.log(disc)
     }
     else if (d === "trainee"){
         disc = 0.5;
@@ -26,20 +28,22 @@ function discount(d,error){
 }
 
 function totalPay(amount){
-    console.log(disc)
     total = 200 * disc * amount;
-    total_pay.innerHTML= `Total: $${total}`;
+    total_pay.innerHTML = `Total a pagar: $${total}`;
 }
 
 function summaryBtn() {
     let ticketsAmount = tickets_amount.value;
     let discountSelect = discount_select.value;
-    console.log("lala")
     discount(discountSelect,error);
     totalPay(ticketsAmount);
 }
 
 function deleteBtn(){
-    
-
+    name_input.value = "";
+    surname_input.value = "";
+    email_input.value = "";
+    tickets_amount.value = "";
+    discount_select.value = "";
+    total_pay.innerHTML = `Total a pagar: $`;
 }
